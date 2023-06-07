ff6ff8fc295296e67f87e5dbe7a7b0f940021f2c
Adrian K <adrian.git@adrianpk.com>
2023-06-07 18:50:43 +0200

    Config

---
+++ internal/sys/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import "flag"
+
+type (
+	Config struct {
+		Log    *LogConfig    `json:"log"`
+		Server *ServerConfig `json:"server"`
+		DB     *DBConfig     `json:"db"`
+	}
+
+	LogConfig struct {
+		Level string `json:"server"`
+	}
+
+	ServerConfig struct {
+		Host string `json:"host"`
+		Port int    `json:"port"`
+	}
+
+	DBConfig struct {
+		Host     string `json:"host"`
+		Port     int    `json:"port"`
+		Username string `json:"username"`
+		Password string `json:"password"`
+		Name     string `json:"name"`
+	}
+)
+
+func Load() *Config {
+	config := &Config{
+		Log:    &LogConfig{},
+		Server: &ServerConfig{},
+		DB:     &DBConfig{},
+	}
+
+	flag.StringVar(&config.Log.Level, "log-level", "info", "Log level")
+	flag.StringVar(&config.Server.Host, "server-host", "localhost", "Server host")
+	flag.IntVar(&config.Server.Port, "server-port", 8080, "Server port")
+	flag.StringVar(&config.DB.Host, "db-host", "localhost", "Database host")
+	flag.IntVar(&config.DB.Port, "db-port", 5432, "Database port")
+	flag.StringVar(&config.DB.Username, "db-username", "admin", "Database username")
+	flag.StringVar(&config.DB.Password, "db-password", "password", "Database password")
+	flag.StringVar(&config.DB.Name, "db-name", "ak", "Database name")
+
+	flag.Parse()
+
+	return config
+}
