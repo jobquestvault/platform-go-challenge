92cb0f9d590fb16190564f93f09a2b9d41e74bbd
Adrian K <adrian.git@adrianpk.com>
2023-06-07 15:52:08 +0200

    Initial model definition

---
+++ internal/app/model.go
@@ -0,0 +1,49 @@
+package app
+
+type (
+	Favorite interface {
+		Faved() bool
+	}
+
+	favorite bool
+)
+
+type Chart struct {
+	ID         string
+	Title      string
+	XAxisTitle string
+	YAxisTitle string
+	Data       []float64
+	favorite
+}
+
+func (i favorite) Faved() bool {
+	return bool(i)
+}
+
+type Insight struct {
+	ID    string
+	Text  string
+	Topic string
+	favorite
+}
+
+type Audience struct {
+	ID                  string
+	Gender              string
+	BirthCountry        string
+	AgeGroup            string
+	HoursSpentOnSocial  int
+	NumPurchasesLastMth int
+	favorite
+}
+
+type Asset[T Favorite] struct {
+	ID   string
+	Name string
+	Data T
+}
+
+func NewAsset[T Favorite](id, name string, data T) Asset[Favorite] {
+	return Asset[Favorite]{ID: id, Name: name, Data: data}
+}
