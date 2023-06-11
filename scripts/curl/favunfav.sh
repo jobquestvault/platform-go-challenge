7da2094e26457a4b3d41899c69bd484cf401406d
Adrian PK <adrianpk@adrianpk.com>
2023-06-11 23:46:31 +0200

    Asset repo interface implemented for PostgreSQL

--- scripts/curl/favunfav.sh
+++ scripts/curl/favunfav.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Usage: ./scripts/curl/fav.sh [action] [asset_type] [asset_id]
+
 # Editable variables
 HOSTNAME="localhost"
 PORT="8080"
-USER="c03dc326-7160-4b63-ac36-7105a4c96fa3"
+USER="efd8cec6-3e45-4fb1-b0d7-3a1be9cfae2c"
 
 # Default values for action and asset ID
 DEFAULT_ACTION="fav"
@@ -11,8 +13,8 @@ DEFAULT_ASSET_ID="32f92457-de96-4f0f-bfd1-9a382a198fd2"
 
 # Command-line parameters
 ACTION=${1:-$DEFAULT_ACTION}
-ASSET_ID=${2:-$DEFAULT_ASSET_ID}
-ASSET_TYPE=${3:-"chart"}
+ASSET_TYPE=${2:-"chart"}
+ASSET_ID=${3:-$DEFAULT_ASSET_ID}
 
 # API endpoint URL
 URL="http://$HOSTNAME:$PORT/api/v1/$USER/assets/$ASSET_ID"
@@ -23,9 +25,14 @@ JSON_BODY=$(cat <<EOF
   "type": "$ASSET_TYPE",
   "action": "$ACTION",
   "name": "Asset Name"
+  "description": "Asset Description"
 }
 EOF
 )
+
+# Echo the URL
+echo "Request URL: $URL"
+echo "JSON Body: $JSON_BODY"
 
 # Make CURL request
 curl -X PUT -H "Content-Type: application/json" -d "$JSON_BODY" "$URL"
